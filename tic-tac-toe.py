ef9bccc33445530024e5e3642d75db7675140947
Kaeun-Lee <lgeun7026@hanmail.net>
2023-09-02 21:51:15 +0900

    feat: Modify to sharing random move list
    change the name of choice_move function to apply_move
    add play game function that computer outputs even index and player1 outputs odd index

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -36,8 +36,8 @@ def print_game_board():
 
 print_game_board()
 
-# 사용자 입력 없이 랜덤으로 고른 위치에 O, X 수를 두고 결과를 출력한다.
-# 한 플레이어가 랜덤으로 정수를 선택하고 그 자리에 O나 X가 없으면 해당하는 수를 둔다.
+# 사용자 입력 없이 랜덤하게 고른 위치에 O, X 수를 두고 결과를 출력한다.
+# 한 플레이어가 랜덤으로 정수를 선택하고 그 자리에 O나 X가 없으면 자신의 수를 둔다.
 
 game_board_position = {
     "1": (0, 0),
@@ -54,33 +54,34 @@ game_board_position = {
 computer_symbol = "O"
 player1_symbol = "X"
 
+board_position = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+random_moves = random.sample(board_position, len(board_position))
 
-def choice_move(player, symbol):
+
+def apply_move(player, symbol, move):
     print(f"{player}님 ({symbol})을 놓을 위치를 선택해 주세요.")
-    player_move = str(random.randint(1, 9))
-    x, y = game_board_position[player_move]
+    print(f"{player}님이 선택한 위치는 {move}입니다.\n")
 
-    if game_board[x][y] not in "OX":
-        print(f"{player}님이 선택한 위치는 {player_move}입니다.\n")
-        game_board[x][y] = symbol
-    else:
-        print("수를 놓을 수 없는 자리입니다. 다시 선택해 주세요.\n")
+    x, y = game_board_position[move]
+    game_board[x][y] = symbol
 
 
-# test
-move_count = 0
-while move_count < 9:
-    choice_move("computer", computer_symbol)
-    print_game_board()
-    choice_move("player1", player1_symbol)
-    print_game_board()
-    move_count += 1
+def play_game():
+    for idx, move in enumerate(random_moves):
+        if idx % 2:
+            apply_move("player1", player1_symbol, move)
+        else:
+            apply_move("computer", computer_symbol, move)
+        print_game_board()
 
 
-print("축하합니다!! computer님이 이겼습니다!\n")
-print("비겼습니다. 게임을 다시 시작해 보세요.")
-print("게임을 다시 하려면 '다시 시작'을 입력해 주세요.\n")
+play_game()
 
-print("게임을 종료하시겠습니까? (y/n)")
-print("게임을 이어서 하려면 '이어서 하기'를 입력해 주세요.\n")
-print(f"{' Tic Tac Toe 게임을 종료합니다. ':*^52}\n")
+
+# print("축하합니다!! computer님이 이겼습니다!\n")
+# print("비겼습니다. 게임을 다시 시작해 보세요.")
+# print("게임을 다시 하려면 '다시 시작'을 입력해 주세요.\n")
+
+# print("게임을 종료하시겠습니까? (y/n)")
+# print("게임을 이어서 하려면 '이어서 하기'를 입력해 주세요.\n")
+# print(f"{' Tic Tac Toe 게임을 종료합니다. ':*^52}\n")
