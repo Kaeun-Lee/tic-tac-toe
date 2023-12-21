2b7f105c711131832c26be42abe28652873adaa0
Kaeun-Lee <lgeun7026@hanmail.net>
2023-12-21 13:01:06 +0900

    Refine previous commit: Distribute reponsibilities of setup_players and remove the function
    - docs: Update comments

--- game/game.py
+++ game/game.py
@@ -9,20 +9,9 @@ class TicTacToeGame:
     """Represents a game of Tic Tac Toe."""
 
     def __init__(self) -> None:
-        self.player1, self.player2 = self.setup_players()
+        self.player1, self.player2 = Human("player1"), Computer("computer")
         self.rounds_count = 1
         self.board = Board()
-
-    def setup_players(self) -> tuple[Player, Player]:
-        """
-        Determines the order of play between two players.
-
-        Returns:
-            The first and second player in the shuffled order.
-        """
-        players = [Human("player1"), Computer("computer")]
-        random.shuffle(players)
-        return players[0], players[1]
 
     def restart_game(self) -> bool:
         """
@@ -54,15 +43,16 @@ class TicTacToeGame:
         print(f"첫 번째 플레이어는 {first_player.name} (O)입니다.")
         print(f"두 번째 플레이어는 {second_player.name} (X)입니다.\n")
 
+        # Cycle through players and their symbols
         symbol_player_pairs = cycle([("O", first_player), ("X", second_player)])
-
         for _ in range(len(self.board._current_state)):
+            # Current player and corresponding symbol
             symbol, current_player = next(symbol_player_pairs)
-            move = current_player.select_move(self.board.get_available_moves())
 
+            move = current_player.select_move(self.board.get_available_moves())
             self.board.apply_turn(move, symbol)
 
-            # Updated game board
+            # Display updated game board
             print(self.board)
 
             if self.board.is_finished():
@@ -71,36 +61,44 @@ class TicTacToeGame:
                 )
                 current_player.wins += 1
                 return
+
         # Board is full, draw game
         print("비겼습니다. 게임을 다시 시작해 보세요.\n")
 
     def run(self):
         """Executes the Tic Tac Toe Game."""
+
+        # Initial welcome message
         print("#" * 60)
         print(f"{'Tic Tac Toe 게임에 오신 걸 환영합니다!':^50}")
         print("#" * 60)
-
         print(
             """
         게임 방식을 선택해 주세요.
         (1) 1 PLAYER
         (2) 2 PLAYER\n"""
         )
-
         print(f"{' Tic Tac Toe 게임을 시작합니다. ':*^52}\n")
-        first_player, second_player = self.player1, self.player2
+
+        # Randomly select player order
+        first_player, second_player = random.sample([self.player1, self.player2], k=2)
 
         while True:
             print(f'"Round {self.rounds_count}"\n')
 
             self.play_one_round(first_player, second_player)
+
+            # Display current score
             print(f"| {f'{self.player1.name} vs. {self.player2.name}':^20} |")
             print(f"| {f'{self.player1.wins} 승        {self.player2.wins} 승':^18} |\n")
 
             if self.restart_game():
+                # Prepare for next round: reset board and switch player order
                 self.rounds_count += 1
                 self.board.reset()
                 first_player, second_player = second_player, first_player
             else:
                 break
+
+        # Goodbye message
         print(f"{' Tic Tac Toe 게임을 종료합니다. ':*^52}\n")
